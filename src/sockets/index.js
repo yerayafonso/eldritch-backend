8dcc61a3e7d2877cdb5dfcbd999af3d8d1870a41
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-10 09:36:09 +0000

    fix remove redundant payload from startGame socket event

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -5,7 +5,7 @@ import { handleDisconnect } from './handleDisconnect.js';
 
 export function loadSocketHandlers(io, socket) {
   socket.on('joinRoom', (payload) => handleJoinRoom(io, socket, payload));
-  socket.on('startGame', (payload) => handleStartGame(io, socket, payload));
+  socket.on('startGame', () => handleStartGame(io, socket));
   socket.on('submitAnswer', (payload) => handleSubmitAnswer(io, socket, payload));
   socket.on('disconnect', () => handleDisconnect(io, socket));
 }
