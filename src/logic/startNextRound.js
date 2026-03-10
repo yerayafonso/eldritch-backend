c745c6098c935faf45d0b86e768922af1461d638
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-10 19:36:38 +0000

    feat: completed startNextRound function

---
+++ src/logic/startNextRound.js
@@ -0,0 +1,49 @@
+import { rooms } from '../rooms.js';
+import { resolveRound } from './resolveRound.js';
+import { ROUND_DURATION_MS } from '../constants.js';
+
+export function startNextRound(io, code) {
+  rooms[code].roundNumber++;
+  rooms[code].currentQuestionIndex++;
+
+  const currentQuestionIndex = rooms[code].currentQuestionIndex;
+  const questionId = rooms[code].questions[currentQuestionIndex].question_id;
+  rooms[code].currentQuestionId = questionId;
+
+  rooms[code].answers = {};
+
+  // start timer: we need roundDeadline property for FE to display countdown and timeout to know when to resolveRound()
+  rooms[code].roundDeadline = Date.now() + ROUND_DURATION_MS;
+
+  rooms[code].timerId = setTimeout(() => {
+    resolveRound(io, code);
+  }, ROUND_DURATION_MS);
+
+  // emit roundStarted - NB we must not send the correct answer, it stays in backend
+  const question = rooms[code].questions[currentQuestionIndex];
+
+  const roundStartedPayload = {
+    monster: {
+      name: rooms[code].monster.name,
+      hp: rooms[code].monsterHp,
+      maxHp: rooms[code].monster.max_hp,
+      image: rooms[code].monster.image,
+    },
+    question: {
+      prompt: question.prompt,
+      options: {
+        a: question.option_a,
+        b: question.option_b,
+        c: question.option_c,
+        d: question.option_d,
+      },
+    },
+    gameState: {
+      teamHp: rooms[code].teamHp,
+      roundNumber: rooms[code].roundNumber,
+      roundDeadline: rooms[code].roundDeadline,
+    },
+  };
+
+  io.to(code).emit('roundStarted', roundStartedPayload);
+}
