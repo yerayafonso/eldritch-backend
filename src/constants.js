124f2a07aee917c6ab1cda9feb16a5b0c7c75bba
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-11 11:55:56 +0000

    fix update questions_per_monster to 10

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const MAX_PLAYERS = 4;
 export const MIN_PLAYERS = 1;
 export const TOTAL_STAGES = 3; // for now it must be left at 3 getMonsterForStage() needs updating before tweaking this
-export const QUESTIONS_PER_MONSTER = 5;
+export const QUESTIONS_PER_MONSTER = 10;
 export const ROUND_DURATION_MS = 15000;
