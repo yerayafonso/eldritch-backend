457e6a6e573479c5acde6a165167bd04e7ca2b71
yerayafonso <yafonso02@gmail.com>
2026-03-16 16:08:38 +0000

    create http GET request endpoint to access data for leaderboard

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import charactersRouter from './endpoints/characters.routes.js';
+import leaderboardRouter from './endpoints/leaderboard.routes.js';
 const app = express();
 
 // placeholder for routes for API endpoints if we add them later down the line
 
 app.use(`/api/characters`, charactersRouter);
+
+app.use(`/api/leaderboard`, leaderboardRouter);
 
 export { app };
