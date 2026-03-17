2527fb41d4021c8c3b1a7d18f1138dcff119c725
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-17 16:08:39 +0000

    feat: implemented disconnection grace period

--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,4 @@ export const ROUND_DURATION_MS = 15000;
 export const DIFFICULTY_MAP = ['easy', 'medium', 'hard'];
 export const ROOM_CLEANUP_DELAY_MS = 180000;
 export const MONSTER_DAMAGE_ADJUSTMENT_FACTOR = 1; // 1 means adding 100% of base damage per extra player, 0.5 would mean adding 50% of base damage per extra player
+export const DISCONNECT_GRACE_PERIOD_MS = 10000;
